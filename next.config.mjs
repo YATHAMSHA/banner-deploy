f5a4fefecae4028162f9dfb5987304a71280ef75
YATHAM <yatham024@gmail.com>
2024-08-06 01:17:39 +0530

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -4,4 +4,5 @@ const isProd = process.env.NODE_ENV === 'production'
 export default {
   assetPrefix: isProd ? '/banner-deploy/' : '',
   basePath: isProd ? '/banner-deploy' : '',
+  trailingSlash: true,
 }
