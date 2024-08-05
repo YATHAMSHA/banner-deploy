60fad292c2dfae179a6c1c6a1559b8b38c8d391d
YATHAM <yatham024@gmail.com>
2024-08-06 01:51:11 +0530

    Updates

---
+++ _next/static/lud8pfUxdfdQONRpynyw6/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/818db46cab7bddb7.css","static/chunks/pages/index-ed8b50bdee0ba4db.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
